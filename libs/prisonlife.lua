941027878094a4eaf03d32c1c54deabd7a6702eb
William Santiago <96854760+Ix1x0x3@users.noreply.github.com>
2022-02-17 13:42:36 -0500

    Update prisonlife.lua

--- libs/prisonlife.lua
+++ libs/prisonlife.lua
@@ -963,6 +963,124 @@ function Kill(plr, method)
 	end
 end
 
+function KillArray(plrs, method)
+	local pooTable = {}
+	local lolTable = {}
+	local pogUsed
+	local SavedCF = LPlayer.Character.HumanoidRootPart.CFrame;
+	local SavedCamCF = GetCamPos();
+	local SavedTeam = GetTeam()
+	local vv = GetTeam();
+	if KillQueue[plr.UserId] ~= nil or KillQueue[plr.UserId] == true then
+		return
+	end;
+	KillQueue[plr.UserId] = true
+	local SwitchedTeams = false;
+	local oldcolor = LPlayer.TeamColor.Name
+	if LPlayer.TeamColor.Name == plr.TeamColor.Name then
+		TeamEvent(OppositeTeam(plr.TeamColor.Name));
+		SwitchedTeams = true;
+	end;
+	if plr.TeamColor.Name == "Medium stone grey" then
+		workspace.Remote.TeamEvent:FireServer("Bright blue")
+	end;
+	local gun = method or "Remington 870"
+	local hadthegun = true;
+	local gunInstance = LPlayer.Backpack:FindFirstChild(gun) or LPlayer.Character:FindFirstChild(gun);
+	if gunInstance == nil then
+		hadthegun = false;
+	end;
+	workspace.Remote.ItemHandler:InvokeServer(workspace.Prison_ITEMS.giver[gun].ITEMPICKUP)
+	repeat wait() until LPlayer.Backpack:FindFirstChild(gun) or LPlayer.Character:FindFirstChild(gun);
+	game.ReplicatedStorage.ReloadEvent:FireServer(LPlayer.Backpack[gun] or LPlayer.Character[gun])
+	repeat wait() until LPlayer.Backpack:FindFirstChild(gun) or LPlayer.Character:FindFirstChild(gun);
+	for _,plr in pairs(plrs) do
+	    local args = {
+			[1] = {
+				[1] = {
+					["RayObject"] = Ray.new(Vector3.new(888.39965820313, 101.48999023438, 2388.4970703125), Vector3.new(-392.79403686523, 32.449165344238, 68.263359069824)),
+					["Distance"] = 23.821922302246,
+					["Cframe"] = CFrame.new(Vector3.new(875.65051269531, 102.33503723145, 2392.59375), Vector3.new(0.99490803480148, -0.09966829419136, -0.014973159879446)),
+					["Hit"] = workspace[plr.Name].Head
+				},
+				[2] = {
+					["RayObject"] = Ray.new(Vector3.new(888.39965820313, 101.48999023438, 2388.4970703125), Vector3.new(-392.48315429688, 33.763328552246, 69.404510498047)),
+					["Distance"] = 23.831678390503,
+					["Cframe"] = CFrame.new(Vector3.new(875.65051269531, 102.37702178955, 2392.6313476563), Vector3.new(0.99450075626373, -0.10315123945475, -0.018112052232027)),
+					["Hit"] = workspace[plr.Name].Head
+				},
+				[3] = {
+					["RayObject"] = Ray.new(Vector3.new(888.39965820313, 101.48999023438, 2388.4970703125), Vector3.new(-393.71801757813, 24.107759475708, 66.369728088379)),
+					["Distance"] = 23.773683547974,
+					["Cframe"] = CFrame.new(Vector3.new(875.65051269531, 102.07206726074, 2392.5297851563), Vector3.new(0.99692684412003, -0.077747754752636, -0.0096018556505442)),
+					["Hit"] = workspace[plr.Name].Head
+				},
+				[4] = {
+					["RayObject"] = Ray.new(Vector3.new(888.39965820313, 101.48999023438, 2388.4970703125), Vector3.new(-395.65017700195, 28.086275100708, 51.692462921143)),
+					["Distance"] = 23.802883148193,
+					["Cframe"] = CFrame.new(Vector3.new(875.65051269531, 102.19207000732, 2392.0632324219), Vector3.new(0.99570387601852, -0.087735749781132, 0.029601030051708)),
+					["Hit"] = workspace[plr.Name].Head
+				},
+				[5] = {
+					["RayObject"] = Ray.new(Vector3.new(888.39965820313, 101.48999023438, 2388.4970703125), Vector3.new(-395.87457275391, 31.009611129761, 48.184143066406)),
+					["Distance"] = 23.82692527771,
+					["Cframe"] = CFrame.new(Vector3.new(875.65051269531, 102.28240203857, 2391.9533691406), Vector3.new(0.99469912052155, -0.095229558646679, 0.038792934268713)),
+					["Hit"] = workspace[plr.Name].Head
+				}
+			},
+			[2] = LPlayer.Backpack:FindFirstChild(gun) or LPlayer.Character:FindFirstChild(gun)
+		}
+		repeat wait() until LPlayer.Backpack:FindFirstChild(gun) or LPlayer.Character:FindFirstChild(gun);
+		game.ReplicatedStorage.ShootEvent:FireServer(unpack(args))
+		repeat wait() until LPlayer.Backpack:FindFirstChild(gun) or LPlayer.Character:FindFirstChild(gun);
+		game.ReplicatedStorage.ShootEvent:FireServer(unpack(args))
+		if SwitchedTeams then
+			workspace.Remote.TeamEvent:FireServer(oldcolor)
+		end;
+		if hadthegun == false then
+			--LPlayer.Backpack:FindFirstChild(gun):Destroy()
+		end;
+		KillQueue[plr.UserId] = nil;
+	end
+	TeamEvent("Bright orange")
+	for i, v in next, LPlayer.Backpack:GetChildren() do
+		if v:IsA("Tool") and v.Name == "AK-47" or v.Name == "Remington 870" or v.Name == "M9" or v.Name == "M4A1" or v.Name == "Taser" then
+			table.insert(pooTable, v.Name)
+			table.insert(lolTable, require(v.GunStates))
+		end
+	end
+	for i, v in next, LPlayer.Character:GetChildren() do
+		if v:IsA("Tool") and v.Name == "AK-47" or v.Name == "Remington 870" or v.Name == "M9" or v.Name == "M4A1" or v.Name == "Taser" then
+			table.insert(pooTable, v.Name)
+			table.insert(lolTable, require(v.GunStates))
+			pogUsed = v.Name;
+		end
+	end
+	if SavedTeam == "Medium stone grey" then
+		TeamEvent("Bright orange")
+	end;
+	workspace.Remote.TeamEvent:FireServer(BrickColor.new(SavedTeam).Name)
+	wait()
+	--LPlayer.Character.ForceField:Destroy();
+	if SavedTeam == "Really red" then
+		Goto(CFrames["Criminal Base"])
+	end;
+	wait()
+	--LPlayer.Character.HumanoidRootPart.CFrame = SavedCF;
+	--workspace.Camera.CFrame = SavedCamCF;
+	for i, v in pairs(pooTable) do
+		GiveItem(v)
+		repeat
+			wait()
+		until LPlayer.Backpack:FindFirstChild(v) or LPlayer.Character:FindFirstChild(v)
+		if pogUsed == v then
+			if LPlayer.Backpack:FindFirstChild(v) then
+				LPlayer.Backpack:FindFirstChild(v).Parent = LPlayer.Character
+			end
+		end
+	end
+end
+
 function Punish(Target)
 	Teleport(Target, CFrame.new(9e9, 9e9, 9e9))
 end
@@ -979,4 +1097,5 @@ return {
   GetTeam = GetTeam;
   LoadChr = LoadChr;
   GiveItem = GiveItem;
+  KArray = KillArray;
 }
