565149979b715e21c26d8542c6dd37621d938c22
William Santiago <96854760+Ix1x0x3@users.noreply.github.com>
2022-03-09 06:16:43 -0600

    Update prisonlife.lua

--- libs/prisonlife.lua
+++ libs/prisonlife.lua
@@ -42,11 +42,13 @@ local punch, shoot, reload, itemGive, teamChange, loadChar =
 	workspace.Remote:FindFirstChild("TeamEvent"),
 	workspace.Remote:FindFirstChild("loadchar")
 local function killPlr(arg1)
+	local character = player.Character
+	local humanoid = character:FindFirstChildWhichIsA("Humanoid")
 	local gunObj = LPlayer.Backpack:FindFirstChild("M9") or (LPlayer.Character and LPlayer.Character:FindFirstChild("M9"))
 	local shootPackets = table.create(0)
 	if not gunObj then
 		itemGive:InvokeServer(workspace.Prison_ITEMS.giver.M9.ITEMPICKUP)
-		gunObj = player.Backpack:FindFirstChild("M9")
+		gunObj = LPlayer.Backpack:FindFirstChild("M9")
 		humanoid:EquipTool(gunObj)
 		gunObj:FindFirstChild("Handle"):BreakJoints()
 		humanoid:UnequipTools()
